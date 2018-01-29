bed9096767484a23ae261521ee983f4f3c761d02
Mars Wong <marswong618@gmail.com>
2018-01-29 05:58:39 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -55,8 +55,8 @@ end
 def adding_matz
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
-  # :known_for => "Ruby",
-  #  :languages => ["LISP", "C"]
+#   :known_for => "Ruby",
+#   :languages => ["LISP", "C"]
 # }
 
 	programmer_hash = {
@@ -77,6 +77,7 @@ def adding_matz
     :known_for => "Ruby",
     :languages => ["LISP", "C"]
   }
+  programmer_hash
 end
 
 def changing_alan
