70f137e54fedf56f010a635e8ce36cce1f918d57
Mars Wong <marswong618@gmail.com>
2018-01-29 05:54:57 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -100,20 +100,19 @@ def changing_alan
 end
 
 def adding_to_dennis
-	programmer_hash =
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
-
+	programmer_hash = {
+    :grace_hopper => {
+      :known_for => "COBOL",
+      :languages => ["COBOL", "FORTRAN"]
+    },
+    :alan_kay => {
+      :known_for => "Object Orientation",
+      :languages => ["Smalltalk", "LISP"]
+    },
+    :dennis_ritchie => {
+      :known_for => "Unix",
+      :languages => ["C"]
+    }
+  }
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 end
